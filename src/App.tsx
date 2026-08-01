e1ad188a6f83602e6b327a23cbd3dbdf6786d5cb
patopinhodoff-crypto <patopinhodoff@gmail.com>
2026-08-01 18:39:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,14 @@
 
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Backrooms Explorer</h1>
+      <p>O sistema está funcionando.</p>
+      <button>Entrar nas Backrooms</button>
+    </div>
+  );
+}
+
+export default App;
