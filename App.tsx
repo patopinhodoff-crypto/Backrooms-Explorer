f1e83809182172627363b9959fc077f02b530e42
patopinhodoff-crypto <patopinhodoff@gmail.com>
2026-08-02 07:27:06 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,32 @@
-import { DesignSystemBrowser } from './preview/DesignSystemBrowser';
+import "./app.css";
 
-function App() {
-  return <DesignSystemBrowser />;
+export default function App() {
+  return (
+    <div className="home">
+
+      <div className="overlay">
+
+        <h1>BACKROOMS</h1>
+        <h2>ARQUIVO CONFIDENCIAL</h2>
+
+        <p>
+          Um banco de dados sobre os níveis desconhecidos das Backrooms.
+          Explore relatos, entidades, documentos e informações coletadas
+          por exploradores.
+        </p>
+
+        <div className="buttons">
+          <button>
+            📂 Explorar Arquivo
+          </button>
+
+          <button>
+            🎮 Explorar os Níveis
+          </button>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
-
-export default App;
